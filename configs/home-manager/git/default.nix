8eba4adf2ace584f0c0d62fa13eb4da7296bca36
Alexandros Liarokapis <liarokapis.v@gmail.com>
2024-12-28 02:55:48 +0200

    add default nix gitignores

--- configs/home-manager/git/default.nix
+++ configs/home-manager/git/default.nix
@@ -8,9 +8,20 @@
         name = "Alexandros Liarokapis";
         email = "liarokapis.v@gmail.com";
       };
+      diff = {
+        tool = "vimdiff";
+      };
+      difftool = {
+        prompt = false;
+      };
       http = {
         postBuffer = 52428800;
       };
     };
+    ignores = [
+      "result"
+      ".envrc"
+      ".direnv"
+    ];
   };
 }
