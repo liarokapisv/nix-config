83871eb46c8e505d5a12c7a6b1573b95959075d0
Alexandros Liarokapis <liarokapis.v@gmail.com>
2026-01-10 15:21:37 -0600

    flatten repo

--- outputs.nix
+++ outputs.nix
@@ -1,1 +1,9 @@
-inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./flake-modules)
+inputs:
+inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+  { lib, ... }:
+  lib.pipe inputs.import-tree [
+    (i: i.filterNot (lib.hasSuffix "flake.nix"))
+    (i: i.filterNot (lib.hasSuffix "outputs.nix"))
+    (i: i ./.)
+  ]
+)
