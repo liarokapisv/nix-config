4ee4c98c1a6fb1987a65fd08b2e5814bdaf6b139
Alexandros Liarokapis <liarokapis.v@gmail.com>
2026-01-16 17:39:51 +0200

    better nixpkgs config handling

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,17 +1,21 @@
 { inputs, ... }:
 {
   flake-file.inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
   perSystem =
-    { system, ... }:
     {
-      _module.args.pkgs = import inputs.nixpkgs {
-        inherit system;
-        config = {
-          allowUnfree = true;
-          segger-jlink.acceptLicense = true;
-          permittedInsecurePackages = [
-            "segger-jlink-qt4-874"
-          ];
+      system,
+      ...
+    }:
+    {
+      imports = [
+        "${inputs.nixpkgs}/nixos/modules/misc/nixpkgs.nix"
+      ];
+
+      config = {
+        nixpkgs = {
+          hostPlatform = system;
+          config.allowUnfree = true;
         };
       };
     };
